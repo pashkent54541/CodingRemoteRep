3583c473445962dfead3eee4be2598a0d49693a8
Pavel <user.name>
2025-01-22 16:56:41 +0300

    Enum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,15 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
-    public static void main(String[] args) {
-        /*
-        * Проверить соответствуют ли элементы массива шаблону e-mail с помощью регулярных выражений
-        String[] emailList = { "test@mail.ru", "123-kkdf@gmail.com", "123@343.com", "123123@yandex.com", "test.test@gmail.com", "sdfsdf@gmail." }
-        * */
-        String[] emailList = {"test@mail.ru", "123-kkdf@gmail.com", "123@343.com", "123123@yandex.com", "test.test@gmail.com", "sdfsdf@gmail."};
-        System.out.println(isValidate(emailList));
+    enum Countries {
+        RUSSIA, SPAIN, FRANCE, GERMAN
     }
 
-    public static HashMap<String, Boolean> isValidate(String[] emails) {
-        HashMap<String, Boolean> map = new HashMap<>();
-
-        for (String mail: emails) {
-            boolean valid = mail.matches("^[A-Za-z0-9\\+_.-]+@[A-Za-z.-]+\\.[A-Za-z]{2,}$");
-            map.put(mail,valid);
+    public static void main(String[] args) {
+        Main.Countries country = Countries.GERMAN;
+        switch (country) {
+            case SPAIN -> System.out.println("Испания");
+            case FRANCE -> System.out.println("Франция");
+            case RUSSIA -> System.out.println("Россия");
+            default -> System.out.println("В классе нет страны");
         }
-        return map;
     }
 }
