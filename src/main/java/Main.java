e15ac1e3b6793a9fadfac8c3c66c257f787f7c36
Pavel <user.name>
2025-01-22 15:47:08 +0300

    регулярка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,21 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        // String[] str = {"Один", "Два"}
-        // Один - 4
-        String[] str = {"Один", "Два"};
-
-        System.out.println(findDuplicate(str));
-
+        /*
+        * Проверить соответствуют ли элементы массива шаблону e-mail с помощью регулярных выражений
+        String[] emailList = { "test@mail.ru", "123-kkdf@gmail.com", "123@343.com", "123123@yandex.com", "test.test@gmail.com", "sdfsdf@gmail." }
+        * */
+        String[] emailList = {"test@mail.ru", "123-kkdf@gmail.com", "123@343.com", "123123@yandex.com", "test.test@gmail.com", "sdfsdf@gmail."};
+        System.out.println(isValidate(emailList));
     }
 
-    public static Map<String,Integer> findDuplicate(String[] strings) {
-        Map<String, Integer> map = new HashMap<>();
+    public static HashMap<String, Boolean> isValidate(String[] emails) {
+        HashMap<String, Boolean> map = new HashMap<>();
 
-        for (String nums : strings) {
-            map.put(nums, nums.length());
+        for (String mail: emails) {
+            boolean valid = mail.matches("^[A-Za-z0-9\\+_.-]+@[A-Za-z.-]+\\.[A-Za-z]{2,}$");
+            map.put(mail,valid);
         }
-
-        /*for(Map.Entry<String,Integer> entry: map.entrySet()) {
-            if(entry.getValue() > 1) {
-                System.out.println(entry.getKey() + " - " + entry.getValue() + " штук");
-            }
-        }*/
-
         return map;
     }
 }
